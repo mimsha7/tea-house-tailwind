b946d7aeae2457d492e3f9aa80d104941fd80975
Md.Mimshadur Rahman <rahman15-6119@diu.edu.bd>
2025-04-28 15:05:54 +0600

    project updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,12 @@ export default {
     content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   
     theme: {
+      marope: ["Manrope", "sans-serif"],
     extend: {
-      }
+      fontFamily: {
+        manrope: ['Manrope', 'sans-serif'], // Add Manrope font
+      },
+     },
     },
   
     plugins: [],
